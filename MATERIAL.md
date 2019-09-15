63ec5c4821500c0062dee3197afe13e5ba74c613
Joe Palala <jose.palala@live.com>
2019-09-15 15:56:34 +0800

    updated todo-service
    get from typicode

---
+++ MATERIAL.md
@@ -0,0 +1,5 @@
+```
+ng generate @angular/material:materialNav --name mainNav
+$ ng generate @angular/material:materialDashboard --name myDashboard
+$ ng generate @angular/material:materialTable -- name myTable
+```
--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Todo  } from '../model/todo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTodos() {
+   const URL = 'https://jsonplaceholder.typicode.com/todos';
+
+    this.http.get(URL)
+    .subscribe((data: any) => {
+      //let i = 1;
+      data.forEach( (todo: {id: number, title: string; completed: boolean; }) => {
+        this.todos.push(new Todo( todo.id, todo.title, todo.completed ));
+      });
+      return this.todos;
+    });
+  }
 }
