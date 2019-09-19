72bf04f94a8fbebf0147ce5bb20b0e8f19534be1
Joe Palala <jose.palala@live.com>
2019-09-19 20:17:41 +0800

    rename services to _services by preference

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TodoListComponent } from './todo-list/todo-list.component';
-import { LoginComponent } from './login/login.component';
+import { TodoComponent } from './todo/todo.component';
+import { HomeComponent } from './home/home.component';
+
+
+import { LoginComponent } from './login';
 
 /* import { MainNavComponent } from './main-nav/main-nav.component';*/
 
 const routes = [
-  { path: '', component: TodoListComponent },
-  { path: 'login', component: LoginComponent }
-
-
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'todos', component: TodoListComponent },
+  { path: 'todo', component: TodoComponent }
 ];
 
 @NgModule({
